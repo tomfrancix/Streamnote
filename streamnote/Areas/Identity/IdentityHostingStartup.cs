f9b0dec4167a5471328b7a4a9fb438b8f3204534
tomfrancix <thomasfahey@mail.com>
2022-05-21 13:21:59 +0100

    Add project files.

---
+++ streamnote/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using streamnote.Data;
+
+[assembly: HostingStartup(typeof(streamnote.Areas.Identity.IdentityHostingStartup))]
+namespace streamnote.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ streamnote/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace streamnote.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
